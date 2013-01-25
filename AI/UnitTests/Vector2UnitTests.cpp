b1e67d373bd8adb5040a6bee4a281a3fdc459127
rwilliams <rwilliams@pc728.(none)>
2013-01-25 00:26:46 +0000

    Started writing unit tests.

---
+++ AI/UnitTests/Vector2UnitTests.cpp
@@ -0,0 +1,65 @@
+#include "Vector2UnitTests.h"
+
+#include "../MathTypes/Vector2.h"
+
+#include <math.h>
+#include <stdio.h>
+
+Vector2UnitTests::Vector2UnitTests()
+{
+	TEST_ADD(Vector2UnitTests::Vector2Equals);
+	TEST_ADD(Vector2UnitTests::Vector2DoesNotEqual);
+	TEST_ADD(Vector2UnitTests::Vector2Addition);
+	TEST_ADD(Vector2UnitTests::Vector2Subtraction);
+	
+	TEST_ADD(Vector2UnitTests::Vector2Distance);
+	TEST_ADD(Vector2UnitTests::Vector2DistanceSquared);
+}
+
+void Vector2UnitTests::Vector2Equals()
+{
+	Vector2 v1(5,9);
+	Vector2 v2(5,9);
+	
+	TEST_ASSERT(v1 == v2);
+}
+
+void Vector2UnitTests::Vector2DoesNotEqual()
+{
+	Vector2 v1(5,9);
+	Vector2 v2(-3,9);
+	
+	TEST_ASSERT(v1 != v2);
+}
+
+void Vector2UnitTests::Vector2Addition()
+{
+	Vector2 v1(5,9);
+	Vector2 v2(-2,3);
+
+	TEST_ASSERT((v1 + v2) == Vector2(3,12));
+}
+
+void Vector2UnitTests::Vector2Subtraction()
+{
+	Vector2 v1(5,9);
+	Vector2 v2(-2,3);
+
+	TEST_ASSERT((v1 - v2) == Vector2(7,6));
+}
+
+void Vector2UnitTests::Vector2Distance()
+{
+	Vector2 v1(3,-10);
+	Vector2 v2(0,-6);
+
+	TEST_ASSERT(v1.Distance(&v2) == 5);
+}
+
+void Vector2UnitTests::Vector2DistanceSquared()
+{
+	Vector2 v1(3,-2);
+	Vector2 v2(3,1);
+
+	TEST_ASSERT(v1.DistanceSquared(&v2) == 9);
+}
---
+++ AI/UnitTests/Vector2UnitTests.h
@@ -0,0 +1,21 @@
+#if !defined(VECTOR2UNITTESTS_H)
+#define VECTOR2UNITTESTS_H
+
+#include "cpptest.h"
+
+class Vector2UnitTests : public Test::Suite
+{
+public:
+	Vector2UnitTests();
+
+private:
+	void Vector2Equals();
+	void Vector2DoesNotEqual();
+	void Vector2Addition();
+	void Vector2Subtraction();
+	
+	void Vector2Distance();
+	void Vector2DistanceSquared();
+};
+
+#endif
