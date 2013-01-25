98e9bfd3c1d060395380d80a667a363dbdf8092f
rwilliams <rwilliams@pc728.(none)>
2013-01-25 20:34:31 +0000

    Now simulate shared memory if being run as standalone binary.

--- AI/main.cpp
+++ AI/main.cpp
@@ -3,28 +3,49 @@
 #include "RobotState.h"
 #include "MathTypes/Vector2.h"
 
+#include "../Shared/SharedMem.h"
+
+#include <string.h>
+
 #if defined(TEST)
 #include "cpptest.h"
 
+#include "UnitTests/AStarUnitTests.h"
 #include "UnitTests/Vector2UnitTests.h"
 #endif
 
 int main(int argc, char *argv[])
 {
-	#if defined(TEST)
+#if defined(STANDALONE)
+
+	// Simulate the shared memory if we're running standalone.
+	TShMem sharedMem;
+
+	// zero-out the shared memory
+    memset(&sharedMem, 0, sizeof(TShMem));
+    // set current state as IDLE
+    sharedMem.systemState = eIDLE;
+
+#endif
+
+#if defined(TEST)
+
+	Test::TextOutput output(Test::TextOutput::Verbose);
+
+	AStarUnitTests aStarUnitTests;
+	aStarUnitTests.run(output, true);
 
 	Vector2UnitTests vector2UnitTests;
-	Test::TextOutput output(Test::TextOutput::Verbose);
 	vector2UnitTests.run(output, true);
 
-	#else
+#else
 
 	AIControl aiControl;
 	
 	aiControl.Initialise();
 	aiControl.RunAI();	
 
-	#endif
+#endif
 	
 	return 0;
 }
