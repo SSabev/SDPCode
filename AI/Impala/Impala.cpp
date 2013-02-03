e5d376497cff0c79755567f9bee0e71ae4f9effb
rwilliams <rwilliams@pc728.(none)>
2013-02-03 19:01:55 +0000

    Added basic path-smoothing implementation based on linear interpolation.

--- AI/Impala/Impala.cpp
+++ AI/Impala/Impala.cpp
@@ -5,7 +5,39 @@ Impala::Impala()
 	
 }
 
-std::list<Vector2> Impala::SmoothPath(std::list<Vector2> aStarPath)
+std::list<Vector2> Impala::SmoothPath(std::list<Vector2> aStarPath, int numAdditionalPasses)
 {
+	std::list<Vector2> smoothedPath = RunPass(aStarPath);
+
+	for (int i=0; i<numAdditionalPasses; i++)
+	{
+		smoothedPath = RunPass(smoothedPath);
+	}
+
+	return smoothedPath;
+}
+
+std::list<Vector2> Impala::RunPass(std::list<Vector2> path)
+{
+	std::vector<Vector2> pathVector(path.begin(), path.end());
+
+	// Currently, our smoothing is going to be as simple as finding the centre 
+	// between each pair of points and using that as part of the smoothed path.
+	std::list<Vector2> smoothedPath;
+
+	smoothedPath.push_front(pathVector.front());
+
+	// Note the size-1, as we only want pairs.
+	for (int i=0; i<pathVector.size()-1; i++)
+	{
+		Vector2 midpointOffset = (pathVector[i+1] - pathVector[i])/2;
+
+		Vector2 smoothedPoint = pathVector[i] + midpointOffset;
 	
+		smoothedPath.push_back(smoothedPoint);
+	}
+
+	smoothedPath.push_back(pathVector.back());
+
+	return smoothedPath;
 }
--- AI/Impala/Impala.h
+++ AI/Impala/Impala.h
@@ -4,13 +4,17 @@
 #include "../MathTypes/Vector2.h"
 
 #include <list>
+#include <vector>
 
 class Impala
 {
 public:
 	Impala();
 	
-	std::list<Vector2> SmoothPath(std::list<Vector2> aStarPath);
+	std::list<Vector2> SmoothPath(std::list<Vector2> aStarPath, int numAdditionalPasses=0);
+
+private:
+	std::list<Vector2> RunPass(std::list<Vector2> path);
 };
 
 #endif
