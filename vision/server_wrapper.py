cb37105d112bfcf64c9f47dc0bbdb0d557b41362
Stefan Sabev <subevstefan@gmail.com>
2013-02-04 12:26:00 +0000

    Adding another branch to make shit work

--- vision/server_wrapper.py
+++ vision/server_wrapper.py
@@ -20,32 +20,32 @@ class RunServer(object):
 
     def run(self):
         while True:
-        print 'Waiting for a connection'
-        self.connection, _ = self.sock.accept()
-        print 'Connection established'
+            print 'Waiting for a connection'
+            self.connection, _ = self.sock.accept()
+            print 'Connection established'
 
-        while True:
-            data = self.pipe.recv()
-            request = self.connection.recv(8)
-            print str(request)
-            if request == 'a':
-                print 'Sending initial data to client'
-                pitch_data = InitSignal(600, 400)
-                s = self.connection.send(pitch_data)
-                print s
-                del(pitch_data)
-            elif request == 'b':
-                print 'Sending data back to the client'
-                # this is navigation data
-                # data = FrameData(2, 3, 4.0, 5, 6, 7.0, 8, 8, 1)
-                self.connection.send(data)
-                del(data)
-            elif request == 'c':
-                print 'Client terminated connection'
-                self.connection.close()
-                break
-            elif request:
-                print request
-            else:
-                print 'No more data'
-                break
+            while True:
+                data = self.pipe.recv()
+                request = self.connection.recv(8)
+                print str(request)
+                if request == 'a':
+                    print 'Sending initial data to client'
+                    pitch_data = InitSignal(600, 400)
+                    s = self.connection.send(pitch_data)
+                    print s
+                    del(pitch_data)
+                elif request == 'b':
+                    print 'Sending data back to the client'
+                    # this is navigation data
+                    # data = FrameData(2, 3, 4.0, 5, 6, 7.0, 8, 8, 1)
+                    self.connection.send(data)
+                    del(data)
+                elif request == 'c':
+                    print 'Client terminated connection'
+                    self.connection.close()
+                    break
+                elif request:
+                    print request
+                else:
+                    print 'No more data'
+                    break
