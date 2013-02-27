abf1482fee26b05db32369e6629ef10953158abf
conn459 <conn459@yahoo.co.uk>
2013-02-27 19:03:40 +0000

    Added tests for DoWeHaveBall()

--- AI/UnitTests/EagleUnitTests.cpp
+++ AI/UnitTests/EagleUnitTests.cpp
@@ -6,10 +6,14 @@
 
 EagleUnitTests::EagleUnitTests()
 {
-  TEST_ADD(EagleUnitTests::EagleIdentifyTarget);
+	TEST_ADD(EagleUnitTests::EagleIdentifyTarget);
     TEST_ADD(EagleUnitTests::EagleIdentifyTargetBallOutOfRangeX);
 	TEST_ADD(EagleUnitTests::EagleIdentifyTargetBallOutOfRangeY);
     TEST_ADD(EagleUnitTests::EagleIdentifyTargetBallOutOfRangeXAndY);
+    TEST_ADD(EagleUnitTests::EagleDoWeHaveBall);
+    TEST_ADD(EagleUnitTests::EagleDoWeHaveBallBoundaryPass);
+    TEST_ADD(EagleUnitTests::EagleDoWeHaveBallBoundaryFail);
+    TEST_ADD(EagleUnitTests::EagleDoWeHaveBallWrapTest);
 }
 
 void EagleUnitTests::EagleIdentifyTarget()
@@ -62,4 +66,52 @@ void EagleUnitTests::EagleIdentifyTargetBallOutOfRangeXAndY()
     Vector2 ball(600, -100);
 
 	TEST_ASSERT(eagle.IdentifyTarget(ourRobotState, enemyRobotState, ball) == (RobotState(Vector2(243, 0),0.0f)));
+}
+
+void EagleUnitTests::EagleDoWeHaveBall()
+{
+	Eagle eagle;
+
+    eagle.SetPitchDimensions(244, 122);
+
+    RobotState ourRobotState(Vector2(10, 10), 1.0f);    
+    Vector2 ball(50, 50);
+
+	TEST_ASSERT(eagle.DoWeHaveBall(ourRobotState, ball) == true);
+}
+
+void EagleUnitTests::EagleDoWeHaveBallBoundaryPass()
+{
+	Eagle eagle;
+
+    eagle.SetPitchDimensions(244, 122);
+
+    RobotState ourRobotState(Vector2(10, 10), 1.56f/*1.5707963267948966f*/);    
+    Vector2 ball(50, 50);
+
+	TEST_ASSERT(eagle.DoWeHaveBall(ourRobotState, ball) == true);
+}
+
+void EagleUnitTests::EagleDoWeHaveBallBoundaryFail()
+{
+	Eagle eagle;
+
+    eagle.SetPitchDimensions(244, 122);
+
+    RobotState ourRobotState(Vector2(10, 10), 1.5707963267948966f);    
+    Vector2 ball(50, 50);
+
+	TEST_ASSERT(eagle.DoWeHaveBall(ourRobotState, ball) == false);
+}
+
+void EagleUnitTests::EagleDoWeHaveBallWrapTest()
+{
+	Eagle eagle;
+
+    eagle.SetPitchDimensions(244, 122);
+
+    RobotState ourRobotState(Vector2(10, 10), 7.283185307179586f);    
+    Vector2 ball(50, 50);
+
+	TEST_ASSERT(eagle.DoWeHaveBall(ourRobotState, ball) == true);
 }
