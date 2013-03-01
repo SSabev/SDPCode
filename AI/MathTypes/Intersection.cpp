0b5949a03ed18768619b96f1b99d64278b979803
rwilliams <rwilliams@pc728.(none)>
2013-03-01 16:31:45 +0000

    Implementing LineCircleIntersection method.

--- AI/MathTypes/Intersection.cpp
+++ AI/MathTypes/Intersection.cpp
@@ -1,5 +1,7 @@
 #include "Intersection.h"
 
+#include <cmath>
+
 Intersection::Intersection()
 {
 
@@ -7,5 +9,20 @@ Intersection::Intersection()
 
 bool Intersection::LineCircleIntersection(Vector2 lineStart, Vector2 lineEnd, Vector2 circleCentre, float circleRadius)
 {
-	// TODO: Add algorithm.
+	// As posted here: http://stackoverflow.com/questions/1073336/circle-line-collision-detection
+	Vector2 directionOfLine = lineEnd - lineStart;
+	Vector2 directionCentreToLineStart = lineStart - circleCentre;
+	
+	float a = directionOfLine.Dot(&directionOfLine);
+	float b = 2*directionCentreToLineStart.Dot(&directionOfLine);
+	float c = directionCentreToLineStart.Dot(&directionCentreToLineStart) - std::pow(circleRadius,2);
+
+	float discriminant = std::pow(b,2) - (4*a) + c;
+
+	if (discriminant >= 0)
+	{
+		return true;
+	}
+
+	return false;
 }
